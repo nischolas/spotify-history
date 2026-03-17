2dca927ab7112def5abceda1704a616fad4a2805
Nicholas Mathieu <nic.schneider@ymail.com>
2026-03-17 10:56:57 +0100

    load umami dynamically

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,11 @@
 import { StrictMode } from "react";
+
+if (import.meta.env.PROD) {
+  const s = document.createElement("script");
+  s.src = "https://umami.nicholas-mathi.eu/script.js";
+  s.dataset.websiteId = "b9e87674-b6ce-4b71-8728-d53ebdaff2d3";
+  document.head.appendChild(s);
+}
 import { createRoot } from "react-dom/client";
 import "@/index.scss";
 import "@/i18n/i18n";
